a8686c316eb901a5774026551a3dd948b0cd6eda
Kamil <tomiczek.kamil26@gmail.com>
2022-04-09 21:23:52 +0200

    fix menu and footer relative and change index.css

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function show(){
-    document.querySelector("header .links").classList.toggle('active')
+
+function show() {
+    let menu = document.querySelector('#menu-bar');
+    let navbar = document.querySelector('.navbar');
+    
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    let menu = document.querySelector('#menu-bar');
+    let navbar = document.querySelector('.navbar');
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
