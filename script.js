8472566682bf07c249d468d63a9f8726dfe6c6ac
unknown <3210xyz@gmail.com>
2022-04-17 20:55:58 +0200

    add 'changing opacity' in picture in mbot.html

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-function Next(){
-    var zd = document.getElementById("zdjecie");
+function Next(){ // Ta funkcja przesuwa opis w stornie mbot.html
     kolejnosc[licz] = 'p';
     licz = licz + 1;
     if(licz == 5){
@@ -34,12 +33,27 @@ function Next(){
     var c3 = document.getElementById("c3");
     var c4 = document.getElementById("c4");
     var c5 = document.getElementById("c5");
+    var z1 = document.getElementById("zdjecie1");
+    var z2 = document.getElementById("zdjecie2");
+    var z3 = document.getElementById("zdjecie3");
+    var z4 = document.getElementById("zdjecie4");
+    var z5 = document.getElementById("zdjecie5");
     licznik ++;
     if(licznik > 5){
-        licznik = 1;
-        zd.src = "../img/"+licznik+".png";
             document.querySelector(".prev").disabled = true
             document.querySelector(".next").disabled = true
+            z5.style.opacity = 0;
+        delay(750).then(() => {
+            z5.style.display = "none"
+            z1.style.transition = "0"
+            delay(10).then(() => {
+            z1.style.display = null
+            z1.style.transition = "0.75s"
+            });
+            delay(100).then(() => {
+            z1.style.opacity = 1;
+            });
+        });
         c1.style.display = "none"
         c1.style.transition = "0"
         c1.style.left = "-800px"        
@@ -55,13 +69,24 @@ function Next(){
                 });
             })
         })
+        licznik = 1;
     }
-    else{
-        zd.src = "../img/"+licznik+".png";
-    }
+
     if(licznik == 2){
             document.querySelector(".prev").disabled = true
             document.querySelector(".next").disabled = true
+            z1.style.opacity = 0;
+            delay(750).then(() => {
+                z1.style.display = "none"
+                z2.style.transition = "0"
+                delay(10).then(() => {
+                    z2.style.display = null
+                    z2.style.transition = "0.75s"
+                    });
+                delay(100).then(() => {
+                    z2.style.opacity = 1;
+                    });
+            });
         c2.style.display = "none"
         c2.style.transition = "0"
         c2.style.left = "-800px"        
@@ -82,6 +107,18 @@ function Next(){
     else if(licznik == 3){
             document.querySelector(".prev").disabled = true
             document.querySelector(".next").disabled = true
+            z2.style.opacity = 0;
+            delay(750).then(() => {
+                z2.style.display = "none"
+                z3.style.transition = "0"
+                delay(10).then(() => {
+                    z3.style.display = null
+                    z3.style.transition = "0.75s"
+                    });
+                delay(100).then(() => {
+                    z3.style.opacity = 1;
+                    });
+            });
         c3.style.display = "none"
         c3.style.transition = "0"
         c3.style.left = "-800px"        
@@ -102,6 +139,18 @@ function Next(){
     else if(licznik == 4){
             document.querySelector(".prev").disabled = true
             document.querySelector(".next").disabled = true
+            z3.style.opacity = 0;
+        delay(750).then(() => {
+            z3.style.display = "none"
+            z4.style.transition = "0"
+            delay(10).then(() => {
+                z4.style.display = null
+                z4.style.transition = "0.75s"
+                });
+            delay(100).then(() => {
+                z4.style.opacity = 1;
+                });
+        });
         c4.style.display = "none"
         c4.style.transition = "0"
         c4.style.left = "-800px"        
@@ -121,6 +170,18 @@ function Next(){
     else if(licznik == 5){
             document.querySelector(".prev").disabled = true
             document.querySelector(".next").disabled = true
+            z4.style.opacity = 0;
+            delay(750).then(() => {
+                z4.style.display = "none"
+                z5.style.transition = "0"
+                delay(10).then(() => {
+                    z5.style.display = null
+                    z5.style.transition = "0.75s"
+                    });
+                delay(100).then(() => {
+                    z5.style.opacity = 1;
+                    });
+            });
         c5.style.display = "none"
         c5.style.transition = "0"
         c5.style.left = "-800px"        
@@ -140,6 +201,11 @@ function Next(){
 }
 
 function Back(){
+    var z1 = document.getElementById("zdjecie1");
+    var z2 = document.getElementById("zdjecie2");
+    var z3 = document.getElementById("zdjecie3");
+    var z4 = document.getElementById("zdjecie4");
+    var z5 = document.getElementById("zdjecie5");
     kolejnosc[licz] = 'l';
     licz = licz + 1;
     if(licz == 5){
@@ -151,11 +217,9 @@ function Back(){
             licz = 0;
         }
     }
-    var zd = document.getElementById("zdjecie");
     licznik --;
     if(licznik < 1){
         licznik = 5;
-        zd.src = "../img/"+licznik+".png";
         document.querySelector(".prev").disabled = true
         document.querySelector(".next").disabled = true
         c1.style.left = "-800px"
@@ -173,9 +237,6 @@ function Back(){
                 })
             })
         })
-    }
-    else{
-        zd.src = "../img/"+licznik+".png";
     }
     if(licznik == 1){
         document.querySelector(".prev").disabled = true
